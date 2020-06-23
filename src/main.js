88f97134bbd0818c9cd64123c044445cbfd6953a
Kate Skorija <kateskorija@gmail.com>
2020-06-23 14:06:14 -0500

    separate UI and business logic and rewrite UI logic

--- src/main.js
+++ src/main.js
@@ -3,3 +3,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    const side1 = parseInt($("#firstSide").val());
+    const side2 = parseInt($("#secondSide").val());
+    const side3 = parseInt($("#thirdSide").val());
+
+    let userTriangle = new Triangle(side1, side2, side3);
+
+    $(".triangleType").text(userTriangle.checkType());
+    $("#triangleResult").show();
+
+  });
+
+});
--- src/triangle.js
+++ src/triangle.js
@@ -16,28 +16,3 @@ Triangle.prototype.checkType = function() {
     return "isosceles triangle";
   } 
 };
-
-
-// $(document).ready(function() {
-//   $("#formOne").submit(function(event) {
-//       event.preventDefault();
-//       const a = parseInt($("input#firstSide").val());
-//       const b = parseInt($("input#secondSide").val());
-//       const c = parseInt($("input#thirdSide").val());
-
-//       if (a + b <= c || a + c <= b || c + b <= a) {
-//           alert("Not a triangle!");
-//       } else if (a === b && b === c)  {
-//           $(".triangleType").text("Equilateral");
-//           $("#triangleResult").show();
-//       } else if (a === b || c === b || c === a) {
-//           $(".triangleType").text("Isosceles");
-//           $("#triangleResult").show();
-//       } else {
-//           $(".triangleType").text("Scalene");
-//           $("#triangleResult").show();
-//           }
-
-//   });
-
-// });
