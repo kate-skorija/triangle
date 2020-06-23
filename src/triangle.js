57a62f92006aa670441222d8c17c9b8a598d64de
Kate Skorija <kateskorija@gmail.com>
2020-06-23 13:00:24 -0500

    restructure file tree and add initial webpack plugins

---
+++ src/triangle.js
@@ -0,0 +1,28 @@
+// export function Triangle(side1, side2, side3) {
+
+// }
+
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//       event.preventDefault();
+//       const a = parseInt($("input#firstSide").val());
+//       const b = parseInt($("input#secondSide").val());
+//       const c = parseInt($("input#thirdSide").val());
+
+//       if (a + b <= c || a + c <= b || c + b <= a) {
+//           alert("Not a triangle!");
+//       } else if (a === b && b === c)  {
+//           $(".triangleType").text("Equilateral");
+//           $("#triangleResult").show();
+//       } else if (a === b || c === b || c === a) {
+//           $(".triangleType").text("Isosceles");
+//           $("#triangleResult").show();
+//       } else {
+//           $(".triangleType").text("Scalene");
+//           $("#triangleResult").show();
+//           }
+
+//   });
+
+// });
